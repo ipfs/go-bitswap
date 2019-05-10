d21667a60ddd53a77e7e76dc27f1258ef01adcd9
hannahhoward <hannah@hannahhoward.net>
2019-05-10 16:37:37 -0700

    test(testnet): add missing test of rate limiting
    Utilize the rate limiting infrastrature to demonstrate reordering of messages for slow peers

--- testnet/network_test.go
+++ testnet/network_test.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"sync"
 	"testing"
+	"time"
 
 	bsmsg "github.com/ipfs/go-bitswap/message"
 	bsnet "github.com/ipfs/go-bitswap/network"
 
+	bstestutil "github.com/ipfs/go-bitswap/testutil"
 	blocks "github.com/ipfs/go-block-format"
 	delay "github.com/ipfs/go-ipfs-delay"
 	mockrouting "github.com/ipfs/go-ipfs-routing/mock"
@@ -65,6 +67,76 @@ func TestSendMessageAsyncButWaitForResponse(t *testing.T) {
 	}
 
 	wg.Wait() // until waiter delegate function is executed
+}
+
+type fakeRateLimiter struct {
+	rateLimits []float64
+}
+
+func (rl *fakeRateLimiter) NextRateLimit() float64 {
+	next := rl.rateLimits[0]
+	rl.rateLimits = rl.rateLimits[1:]
+	return next
+}
+func TestRateLimiting(t *testing.T) {
+	rateLimiter := &fakeRateLimiter{rateLimits: []float64{10000, 100000, 100000, 100000}}
+	net := RateLimitedVirtualNetwork(mockrouting.NewServer(), delay.Fixed(0), rateLimiter)
+	slowPeer := testutil.RandIdentityOrFatal(t)
+	fastPeer := testutil.RandIdentityOrFatal(t)
+	waiter := net.Adapter(testutil.RandIdentityOrFatal(t))
+	slowResponder := net.Adapter(slowPeer)
+	fastResponder := net.Adapter(fastPeer)
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	makeResponderFunc := func(responder bsnet.BitSwapNetwork) bsnet.Receiver {
+		return lambda(func(
+			ctx context.Context,
+			fromWaiter peer.ID,
+			msgFromWaiter bsmsg.BitSwapMessage) {
+
+			msgToWaiter := bsmsg.New(true)
+			msgToWaiter.AddBlock(bstestutil.GenerateBlocksOfSize(1, 1000)[0])
+			responder.SendMessage(ctx, fromWaiter, msgToWaiter)
+		})
+	}
+	slowResponder.SetDelegate(makeResponderFunc(slowResponder))
+	fastResponder.SetDelegate(makeResponderFunc(fastResponder))
+
+	responseSenders := make([]peer.ID, 0, 2)
+	waiter.SetDelegate(lambda(func(
+		ctx context.Context,
+		fromResponder peer.ID,
+		msgFromResponder bsmsg.BitSwapMessage) {
+		responseSenders = append(responseSenders, fromResponder)
+		wg.Done()
+	}))
+
+	messageSentAsync := bsmsg.New(true)
+	messageSentAsync.AddBlock(bstestutil.GenerateBlocksOfSize(1, 1000)[0])
+
+	errSending := waiter.SendMessage(
+		context.Background(), slowPeer.ID(), messageSentAsync)
+	if errSending != nil {
+		t.Fatal(errSending)
+	}
+	time.Sleep(5 * time.Millisecond)
+	errSending = waiter.SendMessage(
+		context.Background(), fastPeer.ID(), messageSentAsync)
+	if errSending != nil {
+		t.Fatal(errSending)
+	}
+	wg.Wait()
+
+	if responseSenders[0] != fastPeer.ID() {
+		t.Fatal("Fast peer should have responded first")
+	}
+	if responseSenders[1] != slowPeer.ID() {
+		t.Fatal("Slow peer should have responded second")
+	}
+
 }
 
 type receiverFunc func(ctx context.Context, p peer.ID,
