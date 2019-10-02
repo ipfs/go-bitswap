fda58312204549b5aa8da99e6022f458a3fefce4
Dirk McCormick <dirkmdev@gmail.com>
2019-10-02 14:43:29 -0400

    wip: track session interest and wants in SessionInterestManager

--- sessioninterestmanager/sessioninterestmanager.go
+++ sessioninterestmanager/sessioninterestmanager.go
@@ -2,32 +2,66 @@ package sessioninterestmanager
 
 import (
 	bsswl "github.com/ipfs/go-bitswap/sessionwantlist"
+	blocks "github.com/ipfs/go-block-format"
 
 	cid "github.com/ipfs/go-cid"
 )
 
 type SessionInterestManager struct {
-	wants *bsswl.SessionWantlist
+	interested *bsswl.SessionWantlist
+	wanted *bsswl.SessionWantlist
 }
 
 // New initializes a new SessionInterestManager.
 func New() *SessionInterestManager {
 	return &SessionInterestManager{
-		wants: bsswl.NewSessionWantlist(),
+		interested: bsswl.NewSessionWantlist(),
+		wanted: bsswl.NewSessionWantlist(),
 	}
 }
 
 func (sim *SessionInterestManager) RecordSessionInterest(ses uint64, ks []cid.Cid) {
-	sim.wants.Add(ks, ses)
+	sim.interested.Add(ks, ses)
+	sim.wanted.Add(ks, ses)
 }
 
 func (sim *SessionInterestManager) RemoveSessionInterest(ses uint64) []cid.Cid {
-	return sim.wants.RemoveSession(ses)
+	sim.wanted.RemoveSession(ses)
+	return sim.interested.RemoveSession(ses)
 }
 
-func (sim *SessionInterestManager) InterestedSessions(blocks []cid.Cid, haves []cid.Cid, dontHaves []cid.Cid) []uint64 {
-	ks := make([]cid.Cid, 0, len(blocks)+len(haves)+len(dontHaves))
-	for _, c := range blocks {
+func (sim *SessionInterestManager) RemoveSessionWants(ses uint64, wants []cid.Cid) {
+	sim.wanted.RemoveSessionKeys(ses, wants)
+}
+
+func (sim *SessionInterestManager) FilterSessionInterested(ses uint64, ks []cid.Cid) []cid.Cid {
+	return sim.interested.SessionHas(ses, ks).Keys()
+}
+
+func (sim *SessionInterestManager) SplitWantedUnwanted(blks []blocks.Block) ([]blocks.Block, []blocks.Block) {
+	// Get the wanted block keys
+	ks := make([]cid.Cid, len(blks))
+	for _, b := range blks {
+		ks = append(ks, b.Cid())
+	}
+	wantedKs := sim.wanted.Has(ks)
+
+	// Separate the blocks into wanted and unwanted
+	wantedBlks := make([]blocks.Block, 0, len(blks))
+	notWantedBlks := make([]blocks.Block, 0)
+	for _, b := range blks {
+		if wantedKs.Has(b.Cid()) {
+			wantedBlks = append(wantedBlks, b)
+		} else {
+			notWantedBlks = append(notWantedBlks, b)
+		}
+	}
+	return wantedBlks, notWantedBlks
+}
+
+func (sim *SessionInterestManager) InterestedSessions(blks []cid.Cid, haves []cid.Cid, dontHaves []cid.Cid) []uint64 {
+	ks := make([]cid.Cid, 0, len(blks)+len(haves)+len(dontHaves))
+	for _, c := range blks {
 		ks = append(ks, c)
 	}
 	for _, c := range haves {
@@ -37,5 +71,5 @@ func (sim *SessionInterestManager) InterestedSessions(blocks []cid.Cid, haves []
 		ks = append(ks, c)
 	}
 
-	return sim.wants.SessionsFor(ks)
+	return sim.interested.SessionsFor(ks)
 }
