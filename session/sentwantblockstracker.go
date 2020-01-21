12f4774e9a36f38523a5a9808944c280303b88ed
Dirk McCormick <dirkmdev@gmail.com>
2020-01-21 10:38:46 -0500

    wip: refactor sentWantBlocksTracker

--- session/sentwantblockstracker.go
+++ session/sentwantblockstracker.go
@@ -5,29 +5,29 @@ import (
 	peer "github.com/libp2p/go-libp2p-core/peer"
 )
 
-// sentWantBlocksTracker keeps track of which peers we've sent a  want-block to
+// sentWantBlocksTracker keeps track of which peers we've sent a want-block to
 type sentWantBlocksTracker struct {
-	sentWantBlocks map[peer.ID]*cid.Set
+	sentWantBlocks map[peer.ID]map[cid.Cid]struct{}
 }
 
 func newSentWantBlocksTracker() *sentWantBlocksTracker {
 	return &sentWantBlocksTracker{
-		sentWantBlocks: make(map[peer.ID]*cid.Set),
+		sentWantBlocks: make(map[peer.ID]map[cid.Cid]struct{}),
 	}
 }
 
 func (s *sentWantBlocksTracker) addSentWantBlocksTo(p peer.ID, ks []cid.Cid) {
-	if _, ok := s.sentWantBlocks[p]; !ok {
-		s.sentWantBlocks[p] = cid.NewSet()
+	cids, ok := s.sentWantBlocks[p]
+	if !ok {
+		cids = make(map[cid.Cid]struct{}, len(ks))
+		s.sentWantBlocks[p] = cids
 	}
 	for _, c := range ks {
-		s.sentWantBlocks[p].Add(c)
+		cids[c] = struct{}{}
 	}
 }
 
 func (s *sentWantBlocksTracker) haveSentWantBlockTo(p peer.ID, c cid.Cid) bool {
-	if ks, ok := s.sentWantBlocks[p]; ok {
-		return ks.Has(c)
-	}
-	return false
+	_, ok := s.sentWantBlocks[p][c]
+	return ok
 }
