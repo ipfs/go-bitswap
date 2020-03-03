ad97437c15f3382850fdc92654b78797570c546d
Dirk McCormick <dirkmdev@gmail.com>
2020-03-03 09:35:20 -0500

    docs: add warning about StopTimer()

--- internal/timer/timer.go
+++ internal/timer/timer.go
@@ -4,6 +4,9 @@ import (
 	"time"
 )
 
+// Stop the timer and drain the channel.
+// Note that this should only be called if the channel hasn't already been
+// drained or it may block indefinitely.
 func StopTimer(t *time.Timer) {
 	if !t.Stop() {
 		// Need to drain the timer if Stop() returns false
