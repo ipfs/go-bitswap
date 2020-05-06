9ebed1ba45634168b60d03659f0373de5b481e16
Dirk McCormick <dirkmdev@gmail.com>
2020-05-06 11:45:00 -0400

    test: check wants only sent once

--- internal/fuzz/invariant.go
+++ internal/fuzz/invariant.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 	"time"
 )
@@ -49,6 +50,10 @@ func checkInvariants(logs []map[string]interface{}) error {
 		return err
 	}
 
+	if err := checkWantSentOnce(logs); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -79,7 +84,6 @@ func checkWantBlockCancel(logs []map[string]interface{}) error {
 		if !ok || at.Before(blkAt.at) {
 			peerBlks[c] = &lineAt{lineIdx + 1, at}
 		}
-		// fmt.Println(log)
 	}
 
 	// node id => peer id => cid => timestamp
@@ -113,7 +117,6 @@ func checkWantBlockCancel(logs []map[string]interface{}) error {
 				byNode[to] = byPeer
 			}
 			byPeer[c] = &lineAt{lineIdx + 1, at}
-			// fmt.Println(log)
 		}
 		return res
 	}
@@ -148,6 +151,80 @@ func checkWantBlockCancel(logs []map[string]interface{}) error {
 				if _, ok := peerWants[blk]; ok {
 					if _, ok := sentCancels[localNode][wantTo][blk]; !ok {
 						return fmt.Errorf("Cancel was not sent to %s after receiving block %s (Line %d)", wantTo, blk, rcvdAt.line)
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func checkWantSentOnce(logs []map[string]interface{}) error {
+	// node id => peer id => cid => want type => timestamp => line number
+	wants := make(map[string]map[string]map[string]map[string]map[time.Time]int)
+
+	for lineIdx, log := range logs {
+		if !strings.Contains(log["msg"].(string), "sent message") {
+			continue
+		}
+		if _, ok := log["type"]; !ok {
+			continue
+		}
+		if !strings.HasPrefix(log["type"].(string), "WANT") {
+			continue
+		}
+
+		localNode := log["local"].(string)
+		at := log["ts"].(time.Time)
+		c := log["cid"].(string)
+		to := log["to"].(string)
+		typ := log["type"].(string)
+
+		byNode, ok := wants[localNode]
+		if !ok {
+			byNode = make(map[string]map[string]map[string]map[time.Time]int)
+			wants[localNode] = byNode
+		}
+
+		byPeer, ok := byNode[to]
+		if !ok {
+			byPeer = make(map[string]map[string]map[time.Time]int)
+			byNode[to] = byPeer
+		}
+
+		byCid, ok := byPeer[c]
+		if !ok {
+			byCid = make(map[string]map[time.Time]int)
+			byPeer[c] = byCid
+		}
+
+		byWantType, ok := byCid[typ]
+		if !ok {
+			byWantType = make(map[time.Time]int)
+			byCid[typ] = byWantType
+		}
+
+		byWantType[at] = lineIdx + 1
+	}
+
+	// Check that want-have was only sent once from a node to each of its peers
+	// Check that want-block was only sent once from a node to each of its peers
+	for localNode, byNode := range wants {
+		for sentTo, byPeer := range byNode {
+			for c, byCid := range byPeer {
+				for wantType, byWantType := range byCid {
+					if len(byWantType) > 1 {
+						var times []time.Time
+						for t := range byWantType {
+							times = append(times, t)
+						}
+						sort.Slice(times, func(i, j int) bool { return times[i].Before(times[j]) })
+						msg := "Line %d: %s -> %s %s %s - %s was sent more than once to the same peer "
+						msg += "(Previously sent %s %s to %s on Line %d)"
+						firstLine := byWantType[times[0]]
+						secondLine := byWantType[times[1]]
+						return fmt.Errorf(msg, firstLine, localNode, sentTo, wantType, c, wantType, wantType, c, sentTo, secondLine)
 					}
 				}
 			}
