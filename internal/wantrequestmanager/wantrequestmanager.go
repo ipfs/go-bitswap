966461c4907534a03116f6f1b8b21fb34ccdb201
Dirk McCormick <dirkmdev@gmail.com>
2020-06-08 17:22:34 -0400

    test: WantRequestManager tests

--- internal/wantrequestmanager/wantrequestmanager.go
+++ internal/wantrequestmanager/wantrequestmanager.go
@@ -1,4 +1,4 @@
-package wrm
+package wantrequestmanager
 
 import (
 	"context"
--- internal/wantrequestmanager/wantrequestmanager_test.go
+++ internal/wantrequestmanager/wantrequestmanager_test.go
@@ -1,204 +1,233 @@
-package wrm
+package wantrequestmanager
 
 import (
 	"context"
+	"sync"
 	"testing"
+	"time"
 
-	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-bitswap/internal/testutil"
+	cid "github.com/ipfs/go-cid"
+	ds "github.com/ipfs/go-datastore"
+	ds_sync "github.com/ipfs/go-datastore/sync"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
 )
 
-func TestWRM(t *testing.T) {
-	ctx := context.Context
-	wm := wrm.New()
-
-	b1 := blocks.NewBlock([]byte("1"))
-	b2 := blocks.NewBlock([]byte("2"))
-
-	n := New()
-	defer n.Shutdown()
-	ch := n.Subscribe(context.Background(), b1.Cid(), b2.Cid())
-
-	n.Publish(b1)
-	blockRecvd, ok := <-ch
-	if !ok {
-		t.Fail()
+func createBlockstore(t *testing.T, ctx context.Context) blockstore.Blockstore {
+	bstore, err := blockstore.CachedBlockstore(ctx,
+		blockstore.NewBlockstore(ds_sync.MutexWrap(ds.NewMapDatastore())),
+		blockstore.DefaultCacheOpts())
+	if err != nil {
+		t.Fatal(err)
 	}
-	assertBlocksEqual(t, b1, blockRecvd)
 
-	n.Publish(b1) // ignored duplicate
+	return bstore
 }
 
-// func TestDuplicates(t *testing.T) {
-// 	b1 := blocks.NewBlock([]byte("1"))
-// 	b2 := blocks.NewBlock([]byte("2"))
+func TestWantRequestManager(t *testing.T) {
+	ctx := context.Background()
+	bstore := createBlockstore(t, ctx)
 
-// 	n := New()
-// 	defer n.Shutdown()
-// 	ch := n.Subscribe(context.Background(), b1.Cid(), b2.Cid())
+	p0 := testutil.GeneratePeers(1)[0]
+	blks := testutil.GenerateBlocksOfSize(3, 8*1024)
+	cids := []cid.Cid{}
+	for _, b := range blks {
+		cids = append(cids, b.Cid())
+	}
 
-// 	n.Publish(b1)
-// 	blockRecvd, ok := <-ch
-// 	if !ok {
-// 		t.Fail()
-// 	}
-// 	assertBlocksEqual(t, b1, blockRecvd)
+	incomingBlks := blks[:2]
 
-// 	n.Publish(b1) // ignored duplicate
+	wg := sync.WaitGroup{}
+	wrm := New(bstore)
 
-// 	n.Publish(b2)
-// 	blockRecvd, ok = <-ch
-// 	if !ok {
-// 		t.Fail()
-// 	}
-// 	assertBlocksEqual(t, b2, blockRecvd)
-// }
+	// Create two want requests that want all the blocks
+	for i := 0; i < 2; i++ {
+		wr, err := wrm.NewWantRequest(cids, func([]cid.Cid) {})
+		if err != nil {
+			t.Fatal(err)
+		}
 
-// func TestPublishSubscribe(t *testing.T) {
-// 	blockSent := blocks.NewBlock([]byte("Greetings from The Interval"))
+		// Listen for the incoming message from each want request
+		incoming := make(chan *IncomingMessage)
+		go wr.Run(ctx, ctx, func(msg *IncomingMessage) {
+			incoming <- msg
+		})
 
-// 	n := New()
-// 	defer n.Shutdown()
-// 	ch := n.Subscribe(context.Background(), blockSent.Cid())
+		// Receive the incoming message
+		go func() {
+			wg.Add(1)
+			defer wg.Done()
 
-// 	n.Publish(blockSent)
-// 	blockRecvd, ok := <-ch
-// 	if !ok {
-// 		t.Fail()
-// 	}
+			select {
+			case <-time.After(10 * time.Millisecond):
+				t.Fatal("timed out")
+			case rcvd := <-incoming:
+				if len(rcvd.Blks) != len(incomingBlks) {
+					t.Fatal("Expected to receive incoming message with blocks")
+				}
+			}
+		}()
 
-// 	assertBlocksEqual(t, blockRecvd, blockSent)
+		// Receive the blocks on the outgoing channel
+		go func() {
+			wg.Add(1)
+			defer wg.Done()
 
-// }
+			for i := 0; i < len(incomingBlks); i++ {
+				select {
+				case <-time.After(10 * time.Millisecond):
+					t.Fatal("timed out")
+				case blk := <-wr.Out:
+					if !testutil.ContainsBlock(incomingBlks, blk) {
+						t.Fatalf("Expected to receive %d incoming messages", len(incomingBlks))
+					}
+				}
+			}
+		}()
+	}
 
-// func TestSubscribeMany(t *testing.T) {
-// 	e1 := blocks.NewBlock([]byte("1"))
-// 	e2 := blocks.NewBlock([]byte("2"))
+	wrm.PublishToSessions(&IncomingMessage{
+		From: p0,
+		Blks: incomingBlks,
+	})
 
-// 	n := New()
-// 	defer n.Shutdown()
-// 	ch := n.Subscribe(context.Background(), e1.Cid(), e2.Cid())
+	wg.Wait()
+}
 
-// 	n.Publish(e1)
-// 	r1, ok := <-ch
-// 	if !ok {
-// 		t.Fatal("didn't receive first expected block")
-// 	}
-// 	assertBlocksEqual(t, e1, r1)
+func TestSubscribeForBlockHaveDontHave(t *testing.T) {
+	ctx := context.Background()
+	bstore := createBlockstore(t, ctx)
 
-// 	n.Publish(e2)
-// 	r2, ok := <-ch
-// 	if !ok {
-// 		t.Fatal("didn't receive second expected block")
-// 	}
-// 	assertBlocksEqual(t, e2, r2)
-// }
+	p0 := testutil.GeneratePeers(1)[0]
+	blks := testutil.GenerateBlocksOfSize(3, 8*1024)
+	cids := []cid.Cid{}
+	for _, b := range blks {
+		cids = append(cids, b.Cid())
+	}
 
-// // TestDuplicateSubscribe tests a scenario where a given block
-// // would be requested twice at the same time.
-// func TestDuplicateSubscribe(t *testing.T) {
-// 	e1 := blocks.NewBlock([]byte("1"))
+	incomingBlks := blks[:1]
+	haves := cids[1:2]
+	dontHaves := cids[2:3]
 
-// 	n := New()
-// 	defer n.Shutdown()
-// 	ch1 := n.Subscribe(context.Background(), e1.Cid())
-// 	ch2 := n.Subscribe(context.Background(), e1.Cid())
+	wrm := New(bstore)
 
-// 	n.Publish(e1)
-// 	r1, ok := <-ch1
-// 	if !ok {
-// 		t.Fatal("didn't receive first expected block")
-// 	}
-// 	assertBlocksEqual(t, e1, r1)
+	wr, err := wrm.NewWantRequest(cids, func([]cid.Cid) {})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	r2, ok := <-ch2
-// 	if !ok {
-// 		t.Fatal("didn't receive second expected block")
-// 	}
-// 	assertBlocksEqual(t, e1, r2)
-// }
+	// Listen for the incoming message from each want request
+	incoming := make(chan *IncomingMessage)
+	go wr.Run(ctx, ctx, func(msg *IncomingMessage) {
+		incoming <- msg
+	})
 
-// func TestShutdownBeforeUnsubscribe(t *testing.T) {
-// 	e1 := blocks.NewBlock([]byte("1"))
+	// Read blocks from the Out channel so the run loop doesn't get stuck
+	go func() {
+		for range wr.Out {
+		}
+	}()
 
-// 	n := New()
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	ch := n.Subscribe(ctx, e1.Cid()) // no keys provided
-// 	n.Shutdown()
-// 	cancel()
+	// Receive the incoming message
+	receiveMessage := func() *IncomingMessage {
+		select {
+		case <-time.After(10 * time.Millisecond):
+			t.Fatal("timed out")
+		case rcvd := <-incoming:
+			return rcvd
+		}
+		return nil
+	}
 
-// 	select {
-// 	case _, ok := <-ch:
-// 		if ok {
-// 			t.Fatal("channel should have been closed")
-// 		}
-// 	case <-time.After(5 * time.Second):
-// 		t.Fatal("channel should have been closed")
-// 	}
-// }
+	// Publish block - should be received by listening want request
+	wrm.PublishToSessions(&IncomingMessage{
+		From: p0,
+		Blks: incomingBlks,
+	})
 
-// func TestSubscribeIsANoopWhenCalledWithNoKeys(t *testing.T) {
-// 	n := New()
-// 	defer n.Shutdown()
-// 	ch := n.Subscribe(context.Background()) // no keys provided
-// 	if _, ok := <-ch; ok {
-// 		t.Fatal("should be closed if no keys provided")
-// 	}
-// }
+	rcvd := receiveMessage()
+	if len(rcvd.Blks) != 1 || !testutil.ContainsBlock(rcvd.Blks, incomingBlks[0]) {
+		t.Fatal("expected to receive block")
+	}
 
-// func TestCarryOnWhenDeadlineExpires(t *testing.T) {
+	// Publish HAVE - should be received by listening want request
+	wrm.PublishToSessions(&IncomingMessage{
+		From:  p0,
+		Haves: haves,
+	})
 
-// 	impossibleDeadline := time.Nanosecond
-// 	fastExpiringCtx, cancel := context.WithTimeout(context.Background(), impossibleDeadline)
-// 	defer cancel()
+	rcvd = receiveMessage()
+	if !testutil.MatchKeysIgnoreOrder(rcvd.Haves, haves) {
+		t.Fatal("expected to receive have")
+	}
 
-// 	n := New()
-// 	defer n.Shutdown()
-// 	block := blocks.NewBlock([]byte("A Missed Connection"))
-// 	blockChannel := n.Subscribe(fastExpiringCtx, block.Cid())
+	// Publish DONT_HAVE - should be received by listening want request
+	wrm.PublishToSessions(&IncomingMessage{
+		From:      p0,
+		DontHaves: dontHaves,
+	})
 
-// 	assertBlockChannelNil(t, blockChannel)
-// }
+	rcvd = receiveMessage()
+	if !testutil.MatchKeysIgnoreOrder(rcvd.DontHaves, dontHaves) {
+		t.Fatal("expected to receive dontHave")
+	}
+}
 
-// func TestDoesNotDeadLockIfContextCancelledBeforePublish(t *testing.T) {
+func TestWantForBlockAlreadyInBlockstore(t *testing.T) {
+	ctx := context.Background()
+	bstore := createBlockstore(t, ctx)
 
-// 	g := blocksutil.NewBlockGenerator()
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	n := New()
-// 	defer n.Shutdown()
+	blk := testutil.GenerateBlocksOfSize(1, 8*1024)[0]
+	cids := []cid.Cid{blk.Cid()}
 
-// 	t.Log("generate a large number of blocks. exceed default buffer")
-// 	bs := g.Blocks(1000)
-// 	ks := func() []cid.Cid {
-// 		var keys []cid.Cid
-// 		for _, b := range bs {
-// 			keys = append(keys, b.Cid())
-// 		}
-// 		return keys
-// 	}()
+	// Put a block into the blockstore
+	bstore.Put(blk)
 
-// 	_ = n.Subscribe(ctx, ks...) // ignore received channel
+	wrm := New(bstore)
 
-// 	t.Log("cancel context before any blocks published")
-// 	cancel()
-// 	for _, b := range bs {
-// 		n.Publish(b)
-// 	}
+	// Create a want request for the block that is already in the blockstore
+	wr, err := wrm.NewWantRequest(cids, func([]cid.Cid) {})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	t.Log("publishing the large number of blocks to the ignored channel must not deadlock")
-// }
+	// Listen for the incoming message
+	incoming := make(chan *IncomingMessage)
+	go wr.Run(ctx, ctx, func(msg *IncomingMessage) {
+		incoming <- msg
+	})
 
-// func assertBlockChannelNil(t *testing.T, blockChannel <-chan blocks.Block) {
-// 	_, ok := <-blockChannel
-// 	if ok {
-// 		t.Fail()
-// 	}
-// }
+	// WantRequestManager should find the block in the blockstore and
+	// immmediately publish a message
+	wg := sync.WaitGroup{}
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
 
-// func assertBlocksEqual(t *testing.T, a, b blocks.Block) {
-// 	if !bytes.Equal(a.RawData(), b.RawData()) {
-// 		t.Fatal("blocks aren't equal")
-// 	}
-// 	if a.Cid() != b.Cid() {
-// 		t.Fatal("block keys aren't equal")
-// 	}
-// }
+		select {
+		case <-time.After(10 * time.Millisecond):
+			t.Fatal("timed out")
+		case rcvd := <-incoming:
+			if len(rcvd.Blks) != 1 || !testutil.ContainsBlock(rcvd.Blks, blk) {
+				t.Fatal("expected to receive block")
+			}
+		}
+	}()
+
+	// Receive the block on the outgoing channel
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+
+		select {
+		case <-time.After(10 * time.Millisecond):
+			t.Fatal("timed out")
+		case b := <-wr.Out:
+			if !b.Cid().Equals(blk.Cid()) {
+				t.Fatal("expected to receive block")
+			}
+		}
+	}()
+
+	wg.Wait()
+}
