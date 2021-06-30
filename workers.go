c557ca3abe61fa06138cfbc747ccf71b627f6f00
Adin Schmahmann <adin.schmahmann@gmail.com>
2021-06-30 15:20:44 -0400

    bump task workers

--- workers.go
+++ workers.go
@@ -14,7 +14,7 @@ import (
 
 // TaskWorkerCount is the total number of simultaneous threads sending
 // outgoing messages
-var TaskWorkerCount = 8
+var TaskWorkerCount = 100
 
 func (bs *Bitswap) startWorkers(ctx context.Context, px process.Process) {
 
