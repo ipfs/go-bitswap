87ff443e336cca6d40db0efcfa2e13ae08965a7d
Gus Eggert <gus@gus.dev>
2022-06-28 15:53:11 -0400

    Basic server implementation
    This is the most barebones implementation possible for a Bitswap
    server. It is missing many things, including but not limited to:
    
    - Only supports /ipfs/bitswap/1.2.0
    - Doesn't support cancellation
    - Doesn't support deny lists

---
+++ server/server.go
@@ -0,0 +1,151 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"sync"
+	"time"
+
+	"github.com/ipfs/go-bitswap/message"
+	bitswap_message_pb "github.com/ipfs/go-bitswap/message/pb"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	ipld "github.com/ipfs/go-ipld-format"
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	"github.com/libp2p/go-msgio"
+	"go.uber.org/zap"
+)
+
+var (
+	ProtocolBitswap protocol.ID = "/ipfs/bitswap/1.2.0"
+	log                         = logging.Logger("bitswap-server")
+	sflog                       = log.Desugar()
+)
+
+type server struct {
+	Host       host.Host
+	Blockstore blockstore.Blockstore
+}
+
+type syncMessage struct {
+	message.BitSwapMessage
+	sync.Mutex
+}
+
+func New(host host.Host, bstore blockstore.Blockstore) (*server, error) {
+	s := &server{
+		Host:       host,
+		Blockstore: bstore,
+	}
+	s.Host.SetStreamHandler(ProtocolBitswap, s.handleNewStream)
+	return s, nil
+}
+
+func (s *server) processWant(ctx context.Context, peer peer.ID, want message.Entry, resp *syncMessage) {
+	blk, err := s.Blockstore.Get(ctx, want.Cid)
+	if err != nil && want.SendDontHave {
+		resp.Lock()
+		resp.AddDontHave(want.Cid)
+		resp.Unlock()
+		var notFound ipld.ErrNotFound
+		if !errors.As(err, &notFound) {
+			sflog.Warn("unexpected error reading from blockstore", zap.Stringer("cid", want.Cid), zap.Error(err))
+			return
+		}
+	}
+
+	switch want.WantType {
+	case bitswap_message_pb.Message_Wantlist_Have:
+		resp.Lock()
+		resp.AddHave(want.Cid)
+		resp.Unlock()
+	case bitswap_message_pb.Message_Wantlist_Block:
+		resp.Lock()
+		resp.AddBlock(blk)
+		resp.Unlock()
+	}
+}
+
+func (s *server) handleNewStream(stream network.Stream) {
+	peer := stream.Conn().RemotePeer()
+	sflog.Debug("bitswap server received new stream", zap.Stringer("peer", peer), zap.String("stream", stream.ID()))
+	defer func() {
+		sflog.Debug("bitswap server closed stream", zap.Stringer("peer", peer), zap.String("stream", stream.ID()))
+		stream.Close()
+	}()
+
+	reader := msgio.NewVarintReaderSize(stream, network.MessageSizeMax)
+	for {
+		received, err := message.FromMsgReader(reader)
+		if err != nil {
+			if err != io.EOF {
+				_ = stream.Reset()
+				sflog.Debug(
+					"bitswap server handleNewStream error",
+					zap.Stringer("from", stream.Conn().RemotePeer()),
+					zap.Error(err),
+				)
+			}
+			// EOF, stream is closed
+			return
+		}
+		peer := stream.Conn().RemotePeer()
+		ctx := context.Background()
+
+		resp := message.New(false)
+		syncResp := &syncMessage{BitSwapMessage: resp}
+		wg := &sync.WaitGroup{}
+		wantList := received.Wantlist()
+		wg.Add(len(wantList))
+		for _, want := range wantList {
+			go func(w message.Entry) {
+				s.processWant(ctx, peer, w, syncResp)
+				wg.Done()
+			}(want)
+		}
+		wg.Wait()
+
+		err = s.writeMsg(ctx, peer, resp)
+		if err != nil {
+			sflog.Warn("error sending response", zap.Error(err))
+			continue
+		}
+	}
+}
+
+func (s *server) writeMsg(ctx context.Context, peer peer.ID, msg message.BitSwapMessage) error {
+	respCtx, respCancel := context.WithTimeout(ctx, time.Second)
+	defer respCancel()
+	deadline, _ := ctx.Deadline()
+
+	// it is required to open a new stream to send a response
+	// TODO: measure impact of not requiring a new stream, to see if we should update the protocol
+	respStream, err := s.Host.NewStream(respCtx, peer, ProtocolBitswap)
+	if err != nil {
+		return fmt.Errorf("opening stream for block response: %w", err)
+	}
+	defer respStream.Close()
+
+	err = respStream.SetWriteDeadline(deadline)
+	if err != nil {
+		return fmt.Errorf("setting stream write deadline: %w", err)
+	}
+	err = msg.ToNetV1(respStream)
+	if err != nil {
+		return fmt.Errorf("sending response: %w", err)
+	}
+	err = respStream.SetWriteDeadline(time.Time{})
+	if err != nil {
+		return fmt.Errorf("resetting response stream deadline: %w", err)
+	}
+	return nil
+}
+
+func (s *server) Close() error {
+	return nil
+}
---
+++ server/server_test.go
@@ -0,0 +1,110 @@
+package server
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/ipfs/go-bitswap"
+	"github.com/ipfs/go-bitswap/message"
+	bitswap_message_pb "github.com/ipfs/go-bitswap/message/pb"
+	bsnet "github.com/ipfs/go-bitswap/network"
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-datastore"
+	dssync "github.com/ipfs/go-datastore/sync"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	mockrouting "github.com/ipfs/go-ipfs-routing/mock"
+	logging "github.com/ipfs/go-log/v2"
+	libp2p "github.com/libp2p/go-libp2p"
+	tnet "github.com/libp2p/go-libp2p-testing/net"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	logging.SetLogLevel("bitswap-server", "debug")
+}
+
+func TestServer(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	bitswapID := tnet.RandIdentityOrFatal(t)
+	bitswapHost, err := libp2p.New(libp2p.Identity(bitswapID.PrivateKey()))
+	require.NoError(t, err)
+	defer bitswapHost.Close()
+
+	// Not using mocknet because its streams don't support deadlines
+	// TODO: add deadlines to mocknet
+	//
+	//	mockNet := mocknet.New(ctx)
+	// bitswapHost, err := mockNet.AddPeer(bitswapID.PrivateKey(), bitswapID.Address())
+	// serverID := tnet.RandIdentityOrFatal(t)
+	//	serverHost, err := mockNet.AddPeer(serverID.PrivateKey(), serverID.Address())
+	// _, err = mockNet.LinkPeers(bitswapHost.ID(), serverHost.ID())
+	// require.NoError(t, err)
+	// _, err = mockNet.ConnectPeers(bitswapHost.ID(), serverHost.ID())
+
+	serverID := tnet.RandIdentityOrFatal(t)
+	serverHost, err := libp2p.New(libp2p.Identity(serverID.PrivateKey()))
+	require.NoError(t, err)
+	defer serverHost.Close()
+
+	rs := mockrouting.NewServer()
+
+	routingClientBitswap := rs.Client(bitswapID)
+
+	bitswapBlockstore := blockstore.NewBlockstore(dssync.MutexWrap(datastore.NewMapDatastore()))
+
+	bsNetwork := bsnet.NewFromIpfsHost(bitswapHost, routingClientBitswap)
+	defer bsNetwork.Stop()
+
+	bs := bitswap.New(ctx, bsNetwork, bitswapBlockstore)
+	defer bs.Close()
+
+	serverBlockstore := blockstore.NewBlockstore(dssync.MutexWrap(datastore.NewMapDatastore()))
+	server, err := New(serverHost, serverBlockstore)
+	require.NoError(t, err)
+	defer server.Close()
+
+	numBlocks := 1000
+	var blks []blocks.Block
+	for i := 0; i < numBlocks; i++ {
+		blks = append(blks, blocks.NewBlock([]byte(fmt.Sprintf("block-%d", i))))
+		err := serverBlockstore.Put(ctx, blks[i])
+		require.NoError(t, err)
+	}
+
+	err = bitswapHost.Connect(ctx, serverHost.Peerstore().PeerInfo(serverHost.ID()))
+
+	require.NoError(t, err)
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < numBlocks; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			received, err := bs.GetBlock(ctx, blks[i].Cid())
+			require.NoError(t, err)
+			require.Equal(t, blks[i].RawData(), received.RawData())
+
+		}(i)
+	}
+	wg.Wait()
+}
+
+func TestThing(t *testing.T) {
+	m := message.New(false)
+
+	buf := make([]byte, 1024)
+	for i := 0; i < 100; i++ {
+		_, err := rand.Reader.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+		blk := blocks.NewBlock(buf)
+		m.AddEntry(blk.Cid(), 1, bitswap_message_pb.Message_Wantlist_Block, true)
+	}
+}
